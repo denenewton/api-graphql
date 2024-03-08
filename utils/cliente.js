9c4d3f8f36790b7d5037dd03ff344b48f1a6377b
Daniel Santos <denenewton@icloud.com>
2024-03-08 16:02:36 -0300

    mudei baseURL en Axios

--- utils/cliente.js
+++ utils/cliente.js
@@ -16,7 +16,7 @@ const headers = {
 };
 
 export const Axios = axios.create({
-  baseURL: "https://app-graphql.vercel.app/api/", //"http://localhost:3000/api/",
+  baseURL: "https://api-graphql-kappa.vercel.app/api/", //"http://localhost:3000/api/",
   method: "post",
   headers: headers,
 });
